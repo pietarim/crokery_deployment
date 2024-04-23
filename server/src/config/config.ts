ef6344f4d0c7e5e38da4f5625d62b5be8de886b9
unknown <pietari.m@hotmail.com>
2024-04-23 18:09:09 +0300

    modify backend config

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,7 +1,7 @@
 const port: string = process.env.PORT || '3001';
 const databaseUrl: string = process.env.DATABASE_URL || 'postgres://postgres:mysecretpassword@localhost:5432/postgres';
 let cookieUrl;
-const corsOriginClientUrl = process.env.CLIENT_URL || 'http://localhost:5432';
+const corsOriginClientUrl = process.env.CLIENT_URL || 'localhost:5432';
 
 enum SameSite {
   Srtict = "strict",
@@ -18,7 +18,7 @@ switch (process.env.NODE_ENV) {
   case 'production':
     cookieIsSameSite = SameSite.Srtict;
     cookieIsSecure = true;
-    corsAllowedOrigins.push(process.env.CLIENT_URL || 'http://localhost:5432');
+    corsAllowedOrigins.push(process.env.CLIENT_URL || 'localhost:5432');
     cookieUrl = process.env.SERVER_URL;
     imageProjectPath = "../images/";
     break;
