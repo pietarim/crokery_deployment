18d03706d1efb159f921b7fd9db111c2f4bd58fc
unknown <pietari.m@hotmail.com>
2024-05-01 22:05:24 +0300

    Add hot reload to Docker setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,11 @@ dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true
+    }
+  },
   plugins: [react()],
   define: {
     __VALUE__: `"${process.env.VALUE}"`
